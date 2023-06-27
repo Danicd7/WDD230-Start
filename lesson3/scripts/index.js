27e16851d63164d88d5900fa4a1be07b7cf08d8f
Danicd7 <danielsilva_88@outlook.com>
2023-06-27 23:22:51 +0200

    commit

--- lesson3/scripts/index.js
+++ lesson3/scripts/index.js
@@ -1,3 +1,7 @@
+const date = new Date();
+const year = date.getFullYear();
+document.querySelector("#currentyear").textContent = year;
+
 document.querySelector(
 	"#lastModified"
 ).textContent = `Last Update: ${document.lastModified}`;
