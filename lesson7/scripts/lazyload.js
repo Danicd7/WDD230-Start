ca9edb0945e63190550454b02af6863c19774c23
Danicd7 <danielsilva_88@outlook.com>
2023-07-04 13:39:43 +0200

    commit

--- lesson7/scripts/lazyload.js
+++ lesson7/scripts/lazyload.js
@@ -1,61 +1,24 @@
-// get all images with data src attribute
-/*const images = document.querySelectorAll("[data-src]");
-
-function preloadImage(img) {
-    const src = img.getAttribute("data-src");
-    if (!src) {
-        return;
-    }
-
-    img.src = src;
-}
+const imagesToLoad = document.querySelectorAll("img[data-src]");
 
 const imgOptions = {
     threshold: 0,
     rootMargin: "0px 0px 50px 0px"
 };
 
-const imgObserver = new IntersectionObserver((entries,
-imgObserver) => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            preloadImage(entry.target);
-            imgObserver.unobserver(entry.target);
-        }
-    });
-}, imgOptions);
-
-images.forEach(image => {
-    imgObserver.observer(image);
-});*/
-
-const imagesToLoad = document.querySelectorAll("img[data-src]");
-
-const imgOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px 20px 0px"
-};
-
 const loadImages = (image) => {
-    image.setAttribute("src", image.getAttribute("data-src"));
-    image.onload = () => {image.removeAttribute("data-src");};
+    image.setAttribute("src", image.getAttribute('data-src'));
+    image.onload = () => {image.removeAttribute('data-src');};
 };
-
-/*imagesToLoad.forEach((img)=> {
-    loadImages(img);
-});*/
 
 if ("IntersectionObserver" in window) {
     const observer = new IntersectionObserver((items, observer) => {
         items.forEach((item) => {
             if (item.isIntersecting) {
-                loadImages(item.target);
-                observer.unobserve(item.target);
+              loadImages(item.target);
+              observer.unobserve(item.target);
             }
         });
-    }, imgOptions);
+    });
     imagesToLoad.forEach((img) => {
         observer.observe(img);
     });
